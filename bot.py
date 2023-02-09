404e3cd040748bee44ae1d04368a1c724c05bd63
Kristina Sharaeva <kristinasharaeva1999@gmail.com>
2023-02-09 18:47:02 +0400

    settings somehow saved

--- bot.py
+++ bot.py
@@ -6,11 +6,30 @@ from telebot import TeleBot
 
 bot = TeleBot(BOT_TOKEN)
 
+city = 'Yerevan'
+
 
 @bot.message_handler(commands=['get_weather'])
 def send_weather(message):
     weather = get_weather()
     bot.send_message(message.chat.id, weather)
+
+
+@bot.message_handler(commands=['setup'])
+def ask_city(message):
+    bot.send_message(message.chat.id, "In what city do you live?")
+    bot.register_next_step_handler(message, proceed_city)
+
+
+def proceed_city(message):
+    bot.send_message(message.chat.id, f"Wow, {message.text} is a great city!")
+    global city
+    city = message.text
+
+
+@bot.message_handler(commands=['get_settings'])
+def get_settings(message):
+    bot.send_message(message.chat.id, city)
 
 
 bot.polling()

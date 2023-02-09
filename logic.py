065bc850dfc1fb8fcba567195a8a136a5c9d9087
Kristina Sharaeva <kristinasharaeva1999@gmail.com>
2023-02-09 19:29:36 +0400

    getting weather for a new city added

--- logic.py
+++ logic.py
@@ -12,10 +12,22 @@ def get_weather(city):
     city_coordinates = cities.get(city)
 
     if city_coordinates is None:
-        return 'Sorry, no such city in database'
+        city_coordinates = get_city_coordinates(city)
+        cities[city] = city_coordinates
 
     weather_endpoint = f'https://api.openweathermap.org/data/2.5/weather?lat={city_coordinates.latitude}&lon={city_coordinates.longitude}&appid={WEATHER_API_KEY}'
     response = requests.get(weather_endpoint)
     weather = response.json()
 
     return f'The weather now in {city}: {weather["weather"][0]["description"]}'
+
+
+def get_city_coordinates(city):
+    city_endpoint = f'http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=1&appid={WEATHER_API_KEY}'
+
+    response = requests.get(city_endpoint)
+    # taking just the first city
+    city = response.json()[0]
+
+    return Coordinates(city['lat'], city['lon'])
+

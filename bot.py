992fc1b1ace7eb828988ece4e17063d569c68e78
Kristina Sharaeva <kristinasharaeva1999@gmail.com>
2023-02-09 18:58:32 +0400

    settings for each user saved separately

--- bot.py
+++ bot.py
@@ -6,7 +6,8 @@ from telebot import TeleBot
 
 bot = TeleBot(BOT_TOKEN)
 
-city = 'Yerevan'
+default_city = 'Yerevan'
+user_cities = {}
 
 
 @bot.message_handler(commands=['get_weather'])
@@ -17,19 +18,19 @@ def send_weather(message):
 
 @bot.message_handler(commands=['setup'])
 def ask_city(message):
-    bot.send_message(message.chat.id, "In what city do you live?")
+    bot.send_message(message.chat.id, 'In what city do you live?')
     bot.register_next_step_handler(message, proceed_city)
 
 
 def proceed_city(message):
-    bot.send_message(message.chat.id, f"Wow, {message.text} is a great city!")
-    global city
-    city = message.text
+    bot.send_message(message.chat.id, f'Wow, {message.text} is a great city!')
+    user_cities[message.chat.id] = message.text
 
 
 @bot.message_handler(commands=['get_settings'])
 def get_settings(message):
-    bot.send_message(message.chat.id, city)
+    user_city = user_cities.get(message.chat.id, default_city)
+    bot.send_message(message.chat.id, user_city)
 
 
 bot.polling()

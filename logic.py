1c9d9ecd3c76f27b26f746ae68c5ac5ab54d947e
Kristina Sharaeva <kristinasharaeva1999@gmail.com>
2023-02-11 19:01:41 +0400

    Weather is fetched to users city

--- logic.py
+++ logic.py
@@ -10,11 +10,19 @@ cities = {'Yerevan': Coordinates(40.178836, 44.50525)}
 
 def get_weather(city):
     if city not in cities:
-        return 'Your city is not supported yet'
+        coordinates = get_coordinates(city)
+        cities[city] = coordinates
 
     coordinates = cities[city]
     url = f'https://api.openweathermap.org/data/2.5/weather?lat={coordinates.latitude}&lon={coordinates.longitude}&appid={WEATHER_API_KEY}'
 
     response = requests.get(url)
     weather = response.json()
-    return f'The weather now in Yerevan: {weather["weather"][0]["description"]}'
+    return f'The weather now in {city}: {weather["weather"][0]["description"]}'
+
+
+def get_coordinates(city):
+    geocode_url = f'http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=1&appid={WEATHER_API_KEY}'
+    response = requests.get(geocode_url)
+    city = response.json()[0]
+    return Coordinates(city['lat'], city['lon'])

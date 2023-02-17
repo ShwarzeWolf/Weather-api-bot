affaa732594a6fa99b06db62c14e6e0bcabe685e
Kristina Sharaeva <kristinasharaeva1999@gmail.com>
2023-02-17 16:40:38 +0400

    moved schema to another file

--- init_db.py
+++ init_db.py
@@ -2,8 +2,7 @@ import sqlite3
 
 connection = sqlite3.connect('database.db')
 
-cursor = connection.cursor()
-cursor.execute("""CREATE TABLE IF NOT EXISTS user_cities (
-                  user_id INTEGER PRIMARY KEY,
-                  city TEXT NOT NULL);
-                  """)
+with open('schema.sql') as f:
+    connection.executescript(f.read())
+
+connection.close()
---
+++ schema.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS user_cities;
+
+CREATE TABLE user_cities (
+    user_id INTEGER PRIMARY KEY,
+    city TEXT NOT NULL
+);

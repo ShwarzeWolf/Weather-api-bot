d0c63202ca9eca32e8dc76e2b89f291bf2921e68
Kristina Sharaeva <kristinasharaeva1999@gmail.com>
2023-02-17 19:47:56 +0400

    database added

--- bot.py
+++ bot.py
@@ -10,13 +10,30 @@ bot = TeleBot(BOT_TOKEN)
 default_city = 'Yerevan'
 cities = {}
 
+import sqlite3
+
 
 def get_user_city(chat_id):
-    return cities.get(chat_id, default_city)
+    connection = sqlite3.connect("database.db")
+    cursor = connection.cursor()
+    cursor.execute("SELECT city FROM user_cities WHERE chat_id = (?)", (chat_id,))
+    city = cursor.fetchone()
+    return city[0] if city else default_city
 
 
 def save_user_city(chat_id, city):
-    cities[chat_id] = city
+    connection = sqlite3.connect("database.db")
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT city FROM user_cities WHERE chat_id = (?)", (chat_id,))
+    result = cursor.fetchone()
+
+    if not result:
+        cursor.execute("INSERT INTO user_cities VALUES (?, ?)", (chat_id, city))
+    else:
+        cursor.execute("UPDATE user_cities SET city = (?) WHERE chat_id = (?)", (city, chat_id))
+
+    connection.commit()
 
 
 @bot.message_handler(commands=['get_weather'])

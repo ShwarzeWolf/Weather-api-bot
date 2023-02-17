5bcf007ea94e36c7d50e8f9194fa5830d7915cb8
Kristina Sharaeva <kristinasharaeva1999@gmail.com>
2023-02-17 19:27:33 +0400

    schema moved to separate file

--- init_db.py
+++ init_db.py
@@ -3,9 +3,8 @@ import sqlite3
 connection = sqlite3.connect("database.db")
 
 cursor = connection.cursor()
-cursor.execute("""CREATE TABLE user_cities (
-                chat_id INTEGER PRIMARY KEY, 
-                city TEXT NOT NULL)
-                """)
+
+with open("schema.sql") as init_script:
+    cursor.executescript(init_script.read())
 
 connection.commit()
